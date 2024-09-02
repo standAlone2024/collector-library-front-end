f3f66ddcb1bd9596b66f19135bf6bb502c5739ae
flint2022 <flint2022@icloud.com>
2024-09-03 02:24:30 +0900

    [FEAT] custom Label을 위한 api 추가

---
+++ src/apis/LabelApi.ts
@@ -0,0 +1,77 @@
+import HttpRequests from '@util/HttpRequests';
+import { bookStore, labelStore } from '@store';
+import { printLog } from '@util/Utils';
+
+export const fetchLabelList = async(sectionId: number | undefined) => {
+    if(!sectionId) return;
+    printLog(sectionId);
+
+    labelStore.setLoading(true);
+    try{
+        const response = await HttpRequests.getInstance().get<{labels: ISectionOptLabel[]}>(`/label/${sectionId}`);
+        if(response.labels)
+            labelStore.setLabels(response.labels);
+    }catch(err) {
+        throw err;
+    }finally {
+        labelStore.setLoading(false);
+    }
+}
+
+export const createLabel = async(label : ISectionOptLabel | undefined) => {
+    if(!label) return;
+    printLog(label);
+
+    labelStore.setLoading(true);
+    try{
+        const response = await HttpRequests.getInstance().post<{label: ISectionOptLabel}>(`/label`);
+        if(response.label)
+            labelStore.addLabel(response.label);
+    }catch(err) {
+        throw err;
+    }finally {
+        labelStore.setLoading(false);
+    }
+}
+
+export const updateLabel = async(label: ISectionOptLabel | undefined) => {
+    if(!label) return;
+    printLog(label);
+
+    labelStore.setLoading(true);
+    try{
+        const response = await HttpRequests.getInstance().put<{label: ISectionOptLabel}>(`/label`, label);
+        if(response.label)
+            labelStore.updateLabel(response.label);
+    }catch(err) {
+        throw err;
+    }finally {
+        labelStore.setLoading(false);
+    }
+}
+
+export const deleteLabel = async(id: number | undefined) => {
+    if(!id) return;
+    printLog(id);
+
+    labelStore.setLoading(true);
+    try{
+        const response = await HttpRequests.getInstance().delete<{message: string}>(`/label/${id}`);
+        if(response.message)
+        {
+            printLog('message: ' + response.message);
+            bookStore.deleteBook(id);
+        }
+    }catch(err) {
+        throw err;
+    }finally {
+        labelStore.setLoading(false);
+    }
+}
+
+export interface ISectionOptLabel {
+    id?: number;
+    section_id: number;
+    label_name: string;
+    order: number;
+}
